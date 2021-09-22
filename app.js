d49016ef6d1b3784e9d69337298e943225e92665
younger-k <younger9403@gmail.com>
2021-09-22 14:58:42 +0900

    feat: router

--- app.js
+++ app.js
@@ -12,32 +12,49 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+  const newsFeed = getData(NEWS_URL);
+  const newsList = [];
 
-window.addEventListener('hashchange', function () {
-  const id = location.hash.substr(1);
-  const newsContent = getData(CONTENT_URL.replace('@id', id));
-  
-  const title = document.createElement('h1');
-  title.innerHTML = newsContent.title;
-  
-  content.appendChild(title);
-});
+  newsList.push('<ul>');
 
-for(let i = 0; i < 10; i++) {
-  const div = document.createElement('div');
-  
-  div.innerHTML = `
-    <li>
-      <a href="#${newsFeed[i].id}">
-        ${newsFeed[i].title} (${newsFeed[i].comments_count})
-      </a>
-    </li>
-  `;
-  
-  ul.appendChild(div.firstElementChild);
+  for(let i = 0; i < 10; i++) {
+    newsList.push(`
+      <li>
+        <a href="#${newsFeed[i].id}">
+          ${newsFeed[i].title} (${newsFeed[i].comments_count})
+        </a>
+      </li>
+    `);
+  }
+
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');  // separator default = ','
 }
 
-container.appendChild(ul);
-container.appendChild(content);
+function newsDetail() {
+  const id = location.hash.substr(1);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
+
+  container.innerHTML = `
+    <h1>${newsContent.title}</h1>
+    <div>
+      <a href="#">목록으로</a>
+    </div>
+  `;
+}
+
+function router() {
+  const routePath = location.hash;
+
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+
+router();
