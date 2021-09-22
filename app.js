16136c36035f92f6ed11095edc6b47a4bd0fb56a
younger-k <younger9403@gmail.com>
2021-09-22 14:35:00 +0900

    fix: dom api remove

--- app.js
+++ app.js
@@ -25,14 +25,17 @@ window.addEventListener('hashchange', function () {
 });
 
 for(let i = 0; i < 10; i++) {
-  const li = document.createElement('li');
-  const a = document.createElement('a');
-
-  a.href = `#${newsFeed[i].id}`;
-  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
-
-  li.appendChild(a);
-  ul.appendChild(li);
+  const div = document.createElement('div');
+  
+  div.innerHTML = `
+    <li>
+      <a href="#${newsFeed[i].id}">
+        ${newsFeed[i].title} (${newsFeed[i].comments_count})
+      </a>
+    </li>
+  `;
+  
+  ul.appendChild(div.firstElementChild);
 }
 
 container.appendChild(ul);
