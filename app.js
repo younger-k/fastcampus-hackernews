d5a701490ca09a6f9d321e2f3eb0aeb59bfc70d9
younger-k <younger9403@gmail.com>
2021-09-22 14:15:37 +0900

    feat: fetch content data

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+const container = document.getElementById('root');
 const ajax = new XMLHttpRequest();
+const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
+const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
 ajax.open('GET', NEWS_URL, false);  // 동기적으로 데이터 가져오겠음.
 ajax.send();
@@ -7,10 +10,30 @@ ajax.send();
 const newsFeed = JSON.parse(ajax.response);
 const ul = document.createElement('ul');
 
+window.addEventListener('hashchange', function () {
+  const id = location.hash.substr(1);
+  
+  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
+  ajax.send();
+
+  const newsContent = JSON.parse(ajax.response);
+  
+  const title = document.createElement('h1');
+  title.innerHTML = newsContent.title;
+  
+  content.appendChild(title);
+});
+
 for(let i = 0; i < 10; i++) {
   const li = document.createElement('li');
-  li.innerHTML = `<li>${newsFeed[i].title}</li>`;
+  const a = document.createElement('a');
+
+  a.href = `#${newsFeed[i].id}`;
+  a.innerHTML = `${newsFeed[i].title} (${newsFeed[i].comments_count})`;
+
+  li.appendChild(a);
   ul.appendChild(li);
 }
 
-document.getElementById('root').appendChild(ul);
+container.appendChild(ul);
+container.appendChild(content);
