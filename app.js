91fab001e7fccc357f337433951ceaa812d5525e
younger-k <younger9403@gmail.com>
2021-09-22 14:39:46 +0900

    refactor: duplicated code remove

--- app.js
+++ app.js
@@ -4,19 +4,20 @@ const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
 
-ajax.open('GET', NEWS_URL, false);  // 동기적으로 데이터 가져오겠음.
-ajax.send();
 
-const newsFeed = JSON.parse(ajax.response);
+function getData(url) {
+  ajax.open('GET', url, false);
+  ajax.send();
+
+  return JSON.parse(ajax.response);
+}
+
+const newsFeed = getData(NEWS_URL);
 const ul = document.createElement('ul');
 
 window.addEventListener('hashchange', function () {
   const id = location.hash.substr(1);
-  
-  ajax.open('GET', CONTENT_URL.replace('@id', id), false);
-  ajax.send();
-
-  const newsContent = JSON.parse(ajax.response);
+  const newsContent = getData(CONTENT_URL.replace('@id', id));
   
   const title = document.createElement('h1');
   title.innerHTML = newsContent.title;
